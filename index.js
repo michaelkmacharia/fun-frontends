aa1ea3a26c0d0a1cbb58b3abc7ab2ca0a799fd4c
Michael K Macharia <104422971+universal-projects@users.noreply.github.com>
2022-07-19 16:15:10 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,114 +2,122 @@
 
 `use strict` ;
 
-const projects = [ `hangman-game` , `meditation-app` , `typing-game` ] ;
+const game = document . querySelector ( `#game-container` ) ;
 
-const slides = document . querySelector ( `#slides-container` ) ;
+const guess = document . querySelector ( `#guess` ) ;
 
-let animation_ID ;
+const message = document . querySelector ( `#message` ) ;
 
-let current_index = 0 ;
+const play = document . querySelector ( `#play` ) ;
 
-let current_translate = 0 ;
+const popup = document . querySelector ( `#popup-container` ) ;
 
-let dragging = false ;
+const right_letters = new Array () ;
 
-let previous_translate = 0 ;
+const score = document . querySelector ( `#score` ) ;
 
-let start_position = 0 ;
+const time = document . querySelector ( `#time` ) ;
 
-const animation = ( () =>
+const word_ = document . querySelector ( `#word` ) ;
+
+let countdown_interval ;
+
+let countdown_time = 7 ;
+
+let fillable = true ;
+
+let score_count = 0 ;
+
+let total_time = 0 ;
+
+let word = words [ Math . floor ( Math . random () * words . length ) ] ;
+
+const click = ( () =>
 	{
-		slides . style . transform = `translateX(${ current_translate }px)` ;
-		( dragging ) ? ( requestAnimationFrame ( animation ) ) : ( `` ) ;
+		clearInterval ( countdown_interval ) ;
+		fillable = true ;
+		right_letters . splice ( 0 ) ;
+		word = words [ Math . floor ( Math . random () * words . length ) ] ;
+		display () ;
+		message . innerText = `` ;
+		popup . style . display = `none` ;
+		countdown_time = ( countdown_time === 0 ) ? ( 7 ) : ( countdown_time ) ;
+		countdown_interval = setInterval ( countdown , 1000 ) ;
 		return ;
 	}
 ) ;
 
-const display = ( () =>
+const countdown = ( () =>
 	{
-		projects . forEach ( ( project ) =>
-			{
-				slides . innerHTML += `<div class="slide" id="slide"><a href="./${ project }/index.html" class="link" id="link"><img src="./storage/screenshots/${ project }.png" class="screenshot" id="screenshot" /></a></div>` ;
-				return ;
-			}
-		) ;
-		slides . querySelectorAll ( `#slide` ) . forEach ( ( slide , slide_index ) =>
-			{
-				slide . querySelector ( `#link` ) . addEventListener ( `dragstart` , prevent ) ;
-				slide . querySelector ( `#screenshot` ) . addEventListener ( `dragstart` , prevent ) ;
-				slide . addEventListener ( `mousedown` , start ( slide_index ) ) ;
-				slide . addEventListener ( `mousemove` , move ) ;
-				slide . addEventListener ( `mouseup` , end ) ;
-				slide . addEventListener ( `mouseleave` , end ) ;
-				slide . addEventListener ( `touchstart` , start ( slide_index ) ) ;
-				slide . addEventListener ( `touchmove` , move ) ;
-				slide . addEventListener ( `touchend` , end ) ;
-				slide . addEventListener ( `touchleave` , end ) ;
-				return ;
-			}
-		) ;
-		return ;
-	}
-) ;
-
-const end = ( () =>
-	{
-		cancelAnimationFrame ( animation_ID ) ;
-		dragging = false ;
-		let translation = current_translate - previous_translate ;
-		( ( translation < - 100 ) && ( current_index < ( ( projects . length ) - 1 ) ) ) ? ( current_index ++ ) : ( `` ) ;
-		( ( translation > 100 ) && ( current_index > 0 ) ) ? ( current_index -- ) : ( `` ) ;
-		position () ;
-		slides . classList . remove ( `grabbing` ) ;
-		return ;
-	}
-) ;
-
-const move = ( ( event ) =>
-	{
-		if ( dragging )
+		total_time ++ ;
+		countdown_time -- ;
+		time . innerText = countdown_time ;
+		if ( countdown_time === 0 )
 		{
-			let current_position = ( event . type . includes ( `mouse` ) ) ? ( event . pageX ) : ( event . touches [ 0 ] . clientX ) ;
-			current_translate = ( previous_translate + current_position ) - start_position ;
+			clearInterval ( countdown_interval ) ;
+			fillable = false ;
+			message . innerText = `Game Over!\n\nYour score: ${ score_count } word${ ( score_count === 1 ) ? ( `` ) : ( `s` ) } in ${ total_time } seconds.` ;
+			popup . style . display = `flex` ;
+			total_time = 0 ;
+			score_count = 0 ;
+			score . innerText = score_count ;
 			return ;
 		}
 		return ;
 	}
 ) ;
 
-const position = ( () =>
+const display = ( () =>
 	{
-		current_translate = current_index * ( - window . innerWidth ) ;
-		previous_translate = current_translate ;
-		slides . style . transform = `translateX(${ current_translate }px)` ;
-		return ;
-	}
-) ;
-
-const prevent = ( ( event ) =>
-	{
-		event . preventDefault () ;
-		event . stopPropagation () ;
-		return ;
-	}
-) ;
-
-const start = ( ( slide_index ) =>
-	{
-		return ( ( event ) =>
+		word_ . innerText = word ;
+		guess . innerHTML = `${ word . split ( `` ) . map ( ( letter ) =>
 			{
-				current_index = slide_index ;
-				start_position = ( event . type . includes ( `mouse` ) ) ? ( event . pageX ) : ( event . touches [ 0 ] . clientX ) ;
-				dragging = true ;
-				animation_ID = requestAnimationFrame ( animation ) ;
-				slides . classList . add ( `grabbing` ) ;
+				return ( `<span class="letter">${ right_letters . includes ( letter ) ? letter : `` }</span>` ) ;
+			}
+		) . join ( `` ) }` ;
+		const inner_word = guess . innerText . replace ( /[ \n]/g , `` ) ;
+		if ( inner_word === word )
+		{
+			countdown_time ++ ;
+			time . innerText = countdown_time ;
+			score_count ++ ;
+			score . innerText = score_count ;
+			click () ;
+			return ;
+		}
+		return ;
+	}
+) ;
+
+const keydown = ( ( event ) =>
+	{
+		if ( fillable )
+		{
+			if ( event . keyCode == 32 || ( event . keyCode >= 48 && event . keyCode <= 90 ) || event . keyCode == 173 || event . keyCode == 188 || event . keyCode == 190 || event . keyCode == 191 || event . keyCode == 222 )
+			{
+				const letter = event . key ;
+				if ( word . includes ( letter ) )
+				{
+					if ( ! right_letters . includes ( letter ) )
+					{
+						right_letters . push ( letter ) ;
+						display () ;
+						return ;
+					}
+					return ;
+				}
 				return ;
 			}
-		) ;
+			return ;
+		}
+		return ;
 	}
 ) ;
 
-window . addEventListener ( `resize` , position ) ;
+countdown_interval = setInterval ( countdown , 1000 ) ;
+
+play . addEventListener ( `click` , click ) ;
+
+window . addEventListener ( `keydown` , keydown ) ;
 
 display () ;
